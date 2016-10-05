04e9844f1d1033dabbb085f89660297fe3b16ea3
Emanuele Tamponi <etamponi@fogcreek.com>
2016-10-05 11:02:19 +0200

    implement snapshotter in Go

---
+++ main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"os/exec"
+
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	router := mux.NewRouter()
+	router.HandleFunc("/snapshot/{projectId}", snapshotVolume).Methods("GET")
+	http.ListenAndServe(":8080", router)
+}
+
+func handleMovie(res http.ResponseWriter, req *http.Request) {
+	res.Header().Set("Content-Type", "application/json")
+
+	vars := mux.Vars(req)
+	projectId := vars["projectId"]
+
+	log.Println("Snapshotting volume of project:", projectId)
+
+	cmd := exec.Command("rbd-snapshot", projectId)
+	var out bytes.Buffer
+	cmd.Stdout = &out
+	err := cmd.Run()
+	if err != nil {
+		log.Println(err.Error())
+		http.Error(res, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	outgoingJSON, err := json.Marshal(out.String())
+
+	if err != nil {
+		log.Println(err.Error())
+		http.Error(res, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	fmt.Fprint(res, string(outgoingJSON))
+}
